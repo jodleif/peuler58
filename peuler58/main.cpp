c1cb4e69967921be5a4ff57ec0544211f35f698f
Jo Øivind Gjernes <jodleif@gmail.com>
2015-11-22 18:37:52 +0100

    MISC: clang compability fixes

--- peuler58/main.cpp
+++ peuler58/main.cpp
@@ -7,7 +7,6 @@ int main()
 	std::cout.sync_with_stdio(false);
 	std::cout << "Project Euler - 58\n";
 	bench_timer bt;
-	peuler58::diagonals d;
 	std::cout << "Side length: " << peuler58::find_sidelength() << "\n";
 	return 0;
 }
--- peuler58/miller_rabin.h
+++ peuler58/miller_rabin.h
@@ -4,6 +4,51 @@
 // http://stackoverflow.com/questions/4236673/sample-code-for-fast-primality-testing-in-c-sharp/4236870#4236870 <--- slow.
 namespace millerrabin
 {
+
+	// http://www.analyticsengines.com/developer_blog/64-bit-modular-exponentiation-in-fortran/
+	// Will have trouble with overflow
+	template<typename Integer>
+	Integer mod_power(Integer base, Integer ex, Integer mod)
+	{
+		Integer res{ 1 };
+		Integer base_t = base%mod;
+		while(ex>0) {
+			if(ex&1) {
+				res = res*base_t;
+				res = res%mod;
+			}
+			ex >>= 1;
+			base_t = base_t*base_t;
+			base_t = base_t%mod;
+		}
+		return res%mod;
+	}
+
+	template<typename Integer>
+	Integer mul(Integer a, Integer b, Integer mod)
+	{
+		int i;
+		Integer now = 0;
+		for (i = sizeof(Integer)*8-1;i >= 0;i--) {
+			if ((a >> i) & 1) break;
+		}
+		for (;i >= 0;i--) {
+			now <<= 1;
+			while (now > mod) now -= mod;
+			if ((a >> i) & 1) now += b;
+			while (now > mod) now -= mod;
+		}
+		return now;
+	}
+	// Slow modular power, less prone to overflow?!
+	template<typename Integer>
+	Integer pow(Integer a, Integer p, Integer mod)
+	{
+		if (p == 0) return 1;
+		if (!(p&1)) return pow(mul(a, a, mod), p / 2, mod);
+		return mul(pow(a, p - 1, mod), a, mod);
+	
+	}
 	template<typename Integer>
 	bool is_prime(Integer prime_to_check, std::vector<Integer> list_to_check)
 	{
@@ -27,49 +72,9 @@ namespace millerrabin
 		return true;
 	}
 
-	template<typename Integer>
-	Integer mul(Integer a, Integer b, Integer mod)
-	{
-		int i;
-		Integer now = 0;
-		for (i = sizeof(Integer)*8-1;i >= 0;i--) {
-			if ((a >> i) & 1) break;
-		}
-		for (;i >= 0;i--) {
-			now <<= 1;
-			while (now > mod) now -= mod;
-			if ((a >> i) & 1) now += b;
-			while (now > mod) now -= mod;
-		}
-		return now;
-	}
-	template<typename Integer>
-	Integer pow(Integer a, Integer p, Integer mod)
-	{
-		if (p == 0) return 1;
-		if (!(p&1)) return pow(mul(a, a, mod), p / 2, mod);
-		return mul(pow(a, p - 1, mod), a, mod);
-	}
 
 
-	// http://www.analyticsengines.com/developer_blog/64-bit-modular-exponentiation-in-fortran/
-	// Will have trouble with overflow
-	template<typename Integer>
-	Integer mod_power(Integer base, Integer ex, Integer mod)
-	{
-		Integer res{ 1 };
-		Integer base_t = base%mod;
-		while(ex>0) {
-			if(ex&1) {
-				res = res*base_t;
-				res = res%mod;
-			}
-			ex >>= 1;
-			base_t = base_t*base_t;
-			base_t = base_t%mod;
-		}
-		return res%mod;
-	}
+
 	/*
 	template<typename Integer, typename Int>
 	Integer pow(Integer x, Int n)
